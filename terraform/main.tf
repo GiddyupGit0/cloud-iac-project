2e4e0df7452f022330fa2e961948990375ae317a
GiddyupGit0 <rachel.macri2@myacu.edu.au>
2026-03-29 00:33:09 +1030

    web app update

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_app_service_plan" "app_plan" {
 # App Service (Web App)
 # -----------------------------
 resource "azurerm_app_service" "webapp" {
-  name                = "iac-webapp-958au"
+  name                = "iac-webapp-958au2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.app_plan.id
